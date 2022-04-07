a5446f74bd12e42e284b9acd4f1152a3f3c3c2b8
ColeBardin <cab572@drexel.edu>
2022-04-07 12:33:59 -0400

    added hw3.3 directory and m file

---
+++ hw3.3/homework_3_3.m
@@ -0,0 +1,39 @@
+%% Preclass Homework 3.3
+%% Question 1
+clc
+A = [2, 0, 1; 3, 0, 0; 0, 1, 0; 0, 1, 3];
+I = [0; 3; 6; 0];
+AM = [A, I];
+
+RAM = rref(AM)
+
+%% Question 2
+clc
+A = [1, 0, -3, 0; 1, 8, -5, -2; 1, 6, -6, 0; 3, 7, -7, -1];
+I = [0; 0; 3; 6];
+AM = [A, I];
+
+RAM = rref(AM)
+
+%% Question 3
+clc
+A = [1, 0, 0, -1, 0; 2, 3, 3, -4, -2; 0, 2, 1, -2, -1; 0, 1, 0, 0, 0; 0, 0, 1, 0, -1];
+I = [0; 2; 0; 1; 0];
+AM = [A, I]; 
+
+RAM = rref(AM)
+
+%% Question 7 part 2
+clc
+AM = [1, 3, -1, 3; 1, 5, 3, 27; -3, -6, 5, 11];
+
+RAM = rref(AM)
+
+%% Question 8 part 2
+clc
+AM = [1, 6, -7, 8; 1, 4, 3, 10; 4, 2, 2, -26];
+
+RAM = rref(AM)
+
+
+
