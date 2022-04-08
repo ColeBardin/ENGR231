6f7c3f46db8290296d2b427bffb8f8420e123f89
ColeBardin <cab572@drexel.edu>
2022-04-08 11:24:37 -0400

    all sections for the questions i used

--- exam1_online/exam1.m
+++ exam1_online/exam1.m
@@ -1,2 +1,38 @@
 %% Online Exam 1
-%% Question 1
+%% Question 2
+clc
+AM = [1, 2, 4, -5; 2, 4, 5, -7; 4, 5, 4, -11];
+RAM = rref(AM)
+
+%% Question 5
+clc
+AM = [1, 0, -3, 14; 4, 2, 1, 23; 0, 1, 4, -9]
+RAM = rref(AM)
+
+%% Question 6
+clc
+AM = [1, 2, 4, 7; 3, 6, 8, 9]
+RAM = rref(AM)
+
+%% Question 7
+clc
+AM = [1, -3, 0, 0, -4; 0, 1, -2, 0, -6; 0, 0, 1, -1, 2; 0, 0, 0, 1, 4]
+RAM = rref(AM)
+
+%% Question 9
+clc
+AM = [1, -3, 7, 0; 0, 1, 6, 0; 0, 0, 3, 0]
+RAM = rref(AM)
+
+%% Question 10
+clc
+AM = [1, -2, 8, 0; 0, 1, 5, 0; 0, 0, 3, 0]
+RAM = rref(AM)
+
+%% Question 11
+clc
+AM = [1, 2, 1, 4; 0, 1, -1, 1; 2, 6, 0, 3]
+RAM = rref(AM)
+
+
+
